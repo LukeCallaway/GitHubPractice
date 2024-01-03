d019ecf37ea702f5f6dfab40795a742a299360ab
LukeCallaway <luke@DESKTOP-1EC4U5S>
2024-01-03 06:01:39 -0600

    updated

--- hack-or-snooze-ajax-api/js/main.js
+++ hack-or-snooze-ajax-api/js/main.js
@@ -3,27 +3,19 @@
 // So we don't have to keep re-finding things on page, find DOM elements once:
 
 const $body = $("body");
-
 const $storiesLoadingMsg = $("#stories-loading-msg");
 const $allStoriesList = $("#all-stories-list");
 const $favoritedStories = $("#favorited-stories");
 const $ownStories = $("#my-stories");
 const $storiesContainer = $("#stories-container")
-
-
-// selector that finds all three story lists
 const $storiesLists = $(".stories-list");
-
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
-
 const $submitForm = $("#submit-form");
-
 const $navSubmitStory = $("#nav-submit-story");
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
-
 const $userProfile = $("#user-profile");
 
 /** To make it easier for individual components to show just themselves, this
